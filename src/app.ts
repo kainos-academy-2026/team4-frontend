4264ff1f87ffb1a5543d4a2c4f508153466f1a34
Shay Delaney <shay.delaney@kainos.com>
2026-07-06 16:21:41 +0100

    Scaffold a basic node project for the frontend (#2)
    updated node version from 20 to 22
    updated package.json with script to copy to views, removed unit testing

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { type Express } from "express";
+import nunjucks from "nunjucks";
+import path from "node:path";
+
+import router from "./routes";
+
+const app: Express = express();
+const viewsPath = path.join(__dirname, "..", "src", "views");
+
+nunjucks.configure(viewsPath, {
+  autoescape: true,
+  express: app,
+  noCache: process.env.NODE_ENV !== "production",
+});
+
+app.set("view engine", "njk");
+app.set("views", viewsPath);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+export default app;
