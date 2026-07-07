56f2d078f76c9a2aff3e94c4325175066194e384
Shay Delaney <shay.delaney@kainos.com>
2026-07-07 14:04:55 +0100

    Added kainos branding to the home page

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import path from "node:path";
+import fs from "node:fs";
 import express, { type Express } from "express";
 import nunjucks from "nunjucks";
 
 import router from "./routes";
 
 const app: Express = express();
-const viewsPath = path.join(__dirname, "..", "src", "views");
+const viewsPath = path.join(__dirname, "views");
+const distPublicPath = path.join(__dirname, "public");
+const sourcePublicPath = path.join(__dirname, "..", "public");
+const publicPath = fs.existsSync(distPublicPath)
+	? distPublicPath
+	: sourcePublicPath;
 
 nunjucks.configure(viewsPath, {
 	autoescape: true,
@@ -18,6 +24,7 @@ app.set("views", viewsPath);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
 
 app.use(router);
 
