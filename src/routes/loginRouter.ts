499f78444dae4bf982519de6fbc92fb81a81483d
Kimritaa <Kimrita.udomsaen@kainos.com>
2026-07-14 17:00:11 +0100

    recovery

--- src/routes/loginRouter.ts
+++ src/routes/loginRouter.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
+import { LoginController } from "../controllers/loginController";
 import { getLogin, postLogout } from "../controllers/loginController";
+import { LoginService } from "../services/loginService";
+
+const loginService = new LoginService();
+const loginController = new LoginController(loginService);
 
 const loginRouter = Router();
 
 loginRouter.get("/login", getLogin);
+loginRouter.post("/login", (req, res) => loginController.postLogin(req, res));
 loginRouter.post("/logout", postLogout);
 
 export default loginRouter;
